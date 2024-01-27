d7dcb007ea74450aba4e203c78976c1f34d2dbf7
agustin <agustinsanchezx@gmail.com>
2024-01-27 14:11:18 -0300

    agregando loader y error page

---
+++ src/app/error.jsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+
+export default function ErrorPage({ error }) {
+    console.log(error)
+    return (
+        <div>Esta pagina no existe, porfavor intente nuevamente</div>
+    )
+}
+
