315a11a97b9235fefe243d61adde38c475f0bf2f
agustin <agustinsanchezx@gmail.com>
2024-01-31 21:03:25 -0300

    Agregando persistencia en memoria con localStorage

--- src/app/components/Form.jsx
+++ src/app/components/Form.jsx
@@ -1,11 +1,18 @@
 'use client'
-import React, { useState } from 'react'
-import Data from './Data'
-
+import React, { useEffect, useState } from 'react'
 
 export default function Form({ days }) {
     const [formData, setFormData] = useState({ ejercicio: "", peso: "" })
     const [data, setData] = useState([])
+
+    // Verificar si existe informacion en el localStorage
+    useEffect(() => {
+        const item = localStorage.getItem(`${days}`)
+        const ejercicio = JSON.parse(item)
+        if (ejercicio?.length > 0) {
+            setData(ejercicio)
+        }
+    },[])
 
     // Obtener informacion de los input
     const handleChange = ({ target }) => {
@@ -30,6 +37,10 @@ export default function Form({ days }) {
         setData(newData)
     }
 
+    // Guardando en LocalStorage
+    useEffect(() => {
+        localStorage.setItem(`${days}`, JSON.stringify(data))
+    }, [data]);
 
     return (
         <section>
@@ -53,106 +64,17 @@ export default function Form({ days }) {
                 />
                 <button type="submit" className="p-3 border">Agregar</button>
             </form>
-            <Data infoForm={data} days={days} deleteExercice={deleteExercice} />
+            <div className="flex flex-col justify-center p-2 gap-2">
+                {data.map((x) => (
+                    <>
+                        <div className='flex flex-col justify-center items-center gap-3 p-2 border' key={"key unica"}>
+                            <span>{x.ejercicio}</span>
+                            <span>{x.peso}</span>
+                            <button onClick={() => deleteExercice(x.id)} className="text-red-600">eliminar</button>
+                        </div>
+                    </>
+                ))}
+            </div>
         </section>
     )
 }
-
-
-
-// 'use client'
-// import dynamic from "next/dynamic"
-// import { useEffect, useState } from "react"
-
-// // Creando componente dinámico
-// function ContentCard({ ejercicio, peso }) {
-//   return (
-//     <>
-//       <span>{ejercicio}</span>
-//       <span>{peso}kg</span>
-//     </>
-//   )
-// }
-
-// // Creamos un componente dinámico para que no se renderice en el servidor
-// const DynamicCard = dynamic(async () => ContentCard, { ssr: false })
-
-
-// export default function Form() {
-//   // Estados
-//   const [formData, setFormData] = useState({ ejercicio: "", peso: "" })
-//   const [content, setContent] = useState(() => {
-//     if (typeof window !== undefined) {
-//       const contentFromStorage = window.localStorage?.getItem("content")
-//       if (contentFromStorage) return JSON.parse(contentFromStorage)
-//     }
-//     return []
-//   })
-
-//   // Obtener informacion de los input
-//   const handleChange = ({ target }) => {
-//     setFormData({ ...formData, [target.name]: target.value })
-//   }
-
-//   // Manejar informacion al enviar
-//   const handleSubmit = (e) => {
-//     e.preventDefault()
-//     if (formData.ejercicio !== "" && formData.peso !== "") {
-//       const data = formData
-//       data.id = content.length + 1
-
-//       setFormData({ ejercicio: "", peso: "" })
-//       setContent([...content, data])
-//     }
-//   }
-
-//   // Eliminar ejercicios
-//   const deleteExercice = (id) => {
-//     const newData = content.filter((ej) => ej.id !== id)
-//     setContent(newData)
-//   }
-
-//   useEffect(() => {
-//     if (typeof window !== undefined) {
-//       window.localStorage?.setItem("content", JSON.stringify(content));
-//     }
-//   }, [content]);
-
-
-//   return (
-//     <>
-//       <section>
-//         <div className='h-[100vh] flex flex-col justify-center items-center gap-2' onSubmit={handleSubmit}>
-//           <form className="bg-red-900 flex flex-col gap-3 p-10" >
-//             <h2>formulario para agregar ejercicio y peso</h2>
-//             <input
-//               type="text"
-//               name="ejercicio"
-//               value={formData.ejercicio}
-//               onChange={handleChange}
-//               className="text-black"
-//               required
-//             />
-//             <input
-//               type="text"
-//               name="peso"
-//               value={formData.peso}
-//               onChange={handleChange}
-//               className="text-black"
-//               required
-//             />
-//             <button type="submit" className="p-3 border">Agregar</button>
-//           </form>
-//           <div className="flex flex-col justify-center gap-2">
-//             {content.map((info) => (
-//               <div className="flex justify-center gap-3 bg-slate-800 p-3" key={info.id}>
-//                 <DynamicCard ejercicio={info.ejercicio} peso={info.peso} />
-//                 <button onClick={() => deleteExercice(info.id)} className="text-red-600">eliminar</button>
-//               </div>
-//             ))}
-//           </div>
-//         </div>
-//       </section>
-//     </>
-//   )
-// }
